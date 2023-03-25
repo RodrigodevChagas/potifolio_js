772c77ae58c25ad6c52eba5894299f25055c9dd1
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-03-25 02:10:22 -0300

    limpando codigo com js

--- Index.js
+++ Index.js
@@ -1,27 +1,29 @@
 const carouselIndicators = document.querySelector('.carousel-indicators');
 const carouselInner = document.querySelector('.carousel-inner');
-var swiperEl = document.querySelector(".mySwiper");
+const swiperEl = document.querySelector(".mySwiper");
+const swiperSlide = document.querySelector(".swiperSlide");
+const certifications = []
 
-function getApiGitHub() {
+function getApiGitHub()
+{
     const url = 'https://api.github.com/users/RodrigodevChagas/repos';
     fetch(url)
     .then( async response =>
         {
-            if (!response.ok) {
+            if (!response.ok)
                 throw new Error(response.statusText);
-            }
             var data =  await response.json();
             carouselInner.innerHTML = `
-            <div class="carousel-item active" data-bs-interval="10000">
-                <div class="card w-100">
-                    <div class="card-body">
-                        <h5 class="card-title">${data[0].name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${data[0].url}Programming Language</h6>
-                        <a href="#" class="card-link">${data[0].url}</a>
-                        <a href="#" class="card-link">Linkedin video project link</a>
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <div class="card w-100">
+                        <div class="card-body">
+                            <h5 class="card-title">${data[0].name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${data[0].url}Programming Language</h6>
+                            <a href="#" class="card-link">${data[0].url}</a>
+                            <a href="#" class="card-link">Linkedin video project link</a>
+                        </div>
                     </div>
-                </div>
-            </div>`;
+                </div>`;
             data.map(item =>{
                 carouselIndicators.innerHTML +=`
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${data.indexOf(item)}" class="active"
@@ -39,32 +41,28 @@ function getApiGitHub() {
                     </div>
                 </div>
                 `
-                // li.innerHTML = `
-                // <strong>${item.name}</strong>
-                // <span>${item.url}</span>
-                // <span> Data criação: ${item.created_at}</span>
-                // `
-                // ul.appendChild(li);
-                console.log(data)
+            })
         })
-
-        }).catch(e => console.log(e.message));
+    .catch(e => console.log(e.message));
 }
 
-Object.assign(swiperEl, {
+
+swiperEl.innerHTML += `<swiper-slide>Certifications</swiper-slide>`
+
+Object.assign(swiperEl,
+{
     grabCursor: true,
     effect: "creative",
     creativeEffect: {
-    prev: {
-        shadow: true,
-        translate: [0, 0, -400],
-    },
-    next: {
-        translate: ["100%", 0, 0],
-    },
+        prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+        },
+        next:{
+            translate: ["100%", 0, 0],
+        },
     },
 });
-
 
 swiperEl.initialize()
 getApiGitHub();
