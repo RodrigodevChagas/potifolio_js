155d272a783e8a0355855985e50f2526a398aa47
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-03-25 03:19:55 -0300

    Adicionando 3 swipers aos certificados

--- Index.js
+++ Index.js
@@ -1,8 +1,18 @@
 const carouselIndicators = document.querySelector('.carousel-indicators');
 const carouselInner = document.querySelector('.carousel-inner');
 const swiperEl = document.querySelector(".mySwiper");
+const swiperEl2 = document.querySelector(".mySwiper2");
+const swiperEl3 = document.querySelector(".mySwiper3");
+const swipers = [swiperEl, swiperEl2, swiperEl3];
 const swiperSlide = document.querySelector(".swiperSlide");
-const certifications = []
+const certifications = ['.NET 5 e EF Core: Relacionando Entidades', 'API Rest com .NET 5: Operações essenciais com verbos HTTP', 'Bootstrap 4: criando uma landing page responsiva',
+'HTTP: Entendendo a web por baixo dos panos', 'JavaScript: Conhecendo o Browser e padrões de projeto', 'C# Parte 9: entrada e saída (I/O) com streams', 'C# e orientação a objetos',
+'HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas', 'HTML5 e CSS3 parte 4: Avançando no CSS', 'C# parte 8: List, lambda, linq', 'C# parte 1: Primeiros passos',
+'C# parte 2: Entendendo a Orientação a Objetos', 'C# parte 3: entendendo herança e interface', 'C# parte 4: Entendendo exceções', 'C# parte 5: Bibliotecas DLLs, documentação e usando o NuGet',
+'C# parte 6: Strings, expressões regulares e a classe Object', 'C# parte 7: Array e tipos genéricos', 'Aprenda a programar em Python com Orientação a Objetos', 'HTML5 e CSS3 parte 1: crie uma página da Web',
+'HTML5 e CSS3 parte 2: Posicionamento, listas e navegação', 'Python Brasil: validação de dados no padrão nacional', 'Testes automatizados: TDD com Python', 'Python Collections parte 1: listas e tuplas',
+'Python Collections parte 2: conjuntos e dicionários', 'Python: avançando na linguagem', 'Python: Avançando na orientação a objetos', 'Python: começando com a linguagem', 'Python: entendendo a Orientação a Objetos',
+'String em Python: extraindo informações de uma URL']
 
 function getApiGitHub()
 {
@@ -46,23 +56,39 @@ function getApiGitHub()
     .catch(e => console.log(e.message));
 }
 
-
-swiperEl.innerHTML += `<swiper-slide>Certifications</swiper-slide>`
-
-Object.assign(swiperEl,
-{
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-        prev: {
-            shadow: true,
-            translate: [0, 0, -400],
-        },
-        next:{
-            translate: ["100%", 0, 0],
-        },
-    },
-});
-
-swiperEl.initialize()
+function certificationsSwiper(certifications) {
+    for(var certification = 0; certification <certifications.length; certification++)
+        {
+            swiperEl.innerHTML += `<swiper-slide>${certifications[certification]}</swiper-slide>`
+            swiperEl2.innerHTML += `<swiper-slide>${certifications[certification]}</swiper-slide>`
+            swiperEl3.innerHTML += `<swiper-slide>${certifications[certification]}</swiper-slide>`
+        }
+    }
+function assignToObject(){
+    for(var swiper = 0; swiper < swipers; swiper++)
+    {
+        Object.assign(swipers[swiper],
+            {
+                grabCursor: true,
+                effect: "creative",
+                creativeEffect: {
+                    prev: {
+                        shadow: true,
+                        translate: ["-125%", 0, -800],
+                        rotate: [0, 0, -90],
+                    },
+                    next: {
+                        shadow: true,
+                        translate: ["125%", 0, -800],
+                        rotate: [0, 0, 90],
+                    },
+                },
+        });
+    }
+}
 getApiGitHub();
+certificationsSwiper(certifications);
+assignToObject();
+swiperEl.initialize();
+swiperEl2.initialize();
+swiperEl3.initialize();
