ba6440406abda53f9845502ffb084ce10866875a
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-03-27 00:44:06 -0300

    refatoração de codigo

--- Index.js
+++ Index.js
@@ -36,48 +36,60 @@ function getApiGitHub()
         {
             if (!response.ok)
                 throw new Error(response.statusText);
+
             var data =  await response.json();
-            carouselIndicators.innerHTML =`
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-            `;
-            carouselInner.innerHTML = `
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Here are my projects available on GitHub</h5>
-                        </div>
-                    </div>
-                </div>`;
+            
+            
             data.map((item, index) =>{
-                //let index = data.indexOf(item) + 1;
-                let dateString = data[index].created_at;
-                let date = new Date(dateString);
-                let day = date.getUTCDate().toString().padStart(2, "0");
-                let month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
-                let year = date.getUTCFullYear().toString();
-                let dateFormat = `${month}/${day}/${year}`;
-                carouselIndicators.innerHTML +=`
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${index + 1}" aria-label="Slide ${index + 1 }"></button>
+                const dateString = item.created_at;
+                const date = datePatronization(dateString);
+                const buttonElement = index < 1 ?  `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${index}" class="active"
+                aria-label="Slide ${index+1}"></button>`:`
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${index}" aria-label="Slide ${index + 1 }"></button>
                 `;
-                carouselInner.innerHTML +=`
-                <div class="carousel-item" data-bs-interval="3000">
-                    <div class="card w-100">
-                        <div class="card-body">
-                            <h5 class="card-title">${data[index].name}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Language: ${data[index].language}</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Creation Date: ${dateFormat}</h6>
-                            <a href="${data[index].html_url}" class="card-link">Github Repository</a>
-                            <a href="#" class="card-link">Linkedin video project link</a>
+              
+                if(index < 1){
+                carouselIndicators.innerHTML = buttonElement;
+    
+                carouselInner.innerHTML = `
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <div class="card w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">Here are my projects available on GitHub</h5>
+                                </div>
+                                </div>
+                                </div>`;
+                }
+                else{
+                    carouselIndicators.innerHTML += buttonElement
+
+                    carouselInner.innerHTML +=`
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <div class="card w-100">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Language: ${item.language}</h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Creation Date: ${date}</h6>
+                                <a href="${item.html_url}" class="card-link">Github Repository</a>
+                                <a href="#" class="card-link">Linkedin video project link</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                `;
-            });
+                    `;
+            }});
         })
     .catch(e => console.log(e.message));
 }
 
+function datePatronization(_dateString){
+    let date = new Date(_dateString);
+    let day = date.getUTCDate().toString().padStart(2, "0");
+    let month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    let year = date.getUTCFullYear().toString();
+    dateString = date
+
+    return `${month}/${day}/${year}`;
+}
 function swiperElement(certification, certificationUrl, elements){
     elements.innerHTML += `
     <swiper-slide class="swiperSlide">
@@ -107,9 +119,9 @@ function certificationsSwiper(){
 }
 
 function assignToObject(){
-    for(var swiper = 0; swiper < swipers; swiper++)
+    swipers.map(swiper =>
     {
-        Object.assign(swipers[swiper],
+        Object.assign(swiper,
             {
                 grabCursor: true,
                 effect: "creative",
@@ -126,8 +138,9 @@ function assignToObject(){
                     },
                 },
         });
-    }
+    });
 }
+
 getApiGitHub();
 certificationsSwiper();
 assignToObject();
