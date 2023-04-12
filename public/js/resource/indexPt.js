6879af2841da9b441ef9cd35fcf331e737411de2
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-12 15:50:13 -0300

    fazendo tradução para português

--- public/js/resource/indexPt.js
+++ public/js/resource/indexPt.js
@@ -1,4 +1,41 @@
 export const localizer = {
-  aboutMe: "Sobre mim",
-  Experience: "Experiencia"
+  aboutMeTitle: `SOBRE MIM`,
+  aboutMeText: `Olá, meu nome é <span>Rodrigo R. Chagas</span>, eu sou um desenvolvedor de software de 22 anos,
+  atualmente focado em desenvolvimento backend com <span>Asp.Net</span>. Eu tinha 19 anos quando conheci o mundo
+  da programação enquanto cursava faculdade que me exigia aprendizado sobre o básico de <span>C++</span> e então,
+  <span>Python</span>. Desde então, eu estive estudando para aumentar meu aprendizado como programador,
+  expandi meus horizontes e aprendi como criar páginas web responsivas com <span>HTML 5</span>, <span>CSS 3</span>,
+  <span>Bootstrap</span> e <span>JavaScript</span>.<br>
+  Na segunda metade de 2021, eu me esforcei para aprender mais e mais sobre esse vasto mundo da tecnologia e,
+  no caminho explorei projetos como um <span>Sistema de Login</span>,
+  <span>Sistema Bancário</span>, aplicação <span>Crawler</span> e também, criei e consumi algumas
+  <span>APIs</span>, para citar alguns, e os apresento neste projeto.<br>
+  Entre as tecnologias que tive contato, estão:
+  <span>SQLServer</span>, <span>RabbitMQ</span>, <span>Elasticsearch</span>,
+  <span>Kibana</span>, <span>MLS</span>, <span>Amazon Web Services</span>, <span>Azure
+  DevOpps</span> and <span>Blobb Storage</span>, <span>Git e GitHub</span> e
+  tenho experiência também como <span>QA Analyst</span>.<br>
+  Aqui estão algumas ferramentas .NET que me sinto confortável para trabalhar: <span>manuseio de Cookies</span>, <span>Identity</span>, <span>Entity
+  Framework</span>, <span>Razor/Blazor pages</span>, <span>Jwt Tokens</span>, <span>API REST</span>, <span>autenticação</span> e
+  <span>autorização</span>, é justo pontuar que tenho conhecimento a prática de padrões <span>DDD</span> e <span>TDD</span>,
+  <span>paradigma da POO(Programação Orientada a Objetos)</span>, e uma base sólida em <span>SOLID</span>.<br>
+  Em agosto de 2022, me juntei a "Employer" como estagiário, onde é meu atual trabalho,
+  faço parte do squad BNE, onde aprendi como o ambiente de desenvolvimento para grandes empresas funcionam,
+  como resolver e lidar com problemas.<br>
+  Além da programação e seus mundo de possibilidades, eis algumas qualidades minhas brevemente: Eu sou uma pessoa que fala <span>Inglês Fluente</span>
+  e me vejo como uma pessoa batalhadora, proativa que nunca desiste e ama dissecar situações complicadas e resolvê-las da melhor forma possível,
+  é o que faço desde sempre e planejo assim continuar fazendo.`,
+  experienceTitle: `EXPERIÊNCIA`,
+  experienceText: `Antes de me aventurar no mundo da programação trabalhei como <span>Leiturista</span>, na Tellus,
+  de Janeiro de 2020 à Abril de 2021, indo de casa em casa, se comunicando com centenas de pessoas e as entregando
+  a conta da luz.
+  Lá aprendi como me comunicar bem, ter empatia, e a ser paciente, mas o mais importante, como valorizar o serviço e o levar a sério.<br>
+  Hoje faço parte do time do BNE, que tem 2
+  dos 2 maiores sites de RH no Brasil. Lá vivencio como é desenvolver software a nível profissional, alguns dos projetos que lá desenvolvi desde que
+  entrei <span>Catálogo, CRUD, Carrinho de Compras</span>, colaborei com uma nova <span>ferramenta de desenvolvimento</span> do site da empresa, tive contato com
+  o desenvolvimento do <span>Crawler</span> (extrai vagas de outros sites e trás para o nosso) usando expressões XPath
+  para analisar HTML ou arquivos XML e coletando dados, indexei arquivos em um site <span>LMS</span> usando <span>AWS</span>,
+  documentei e fiz casos de teste de todo o site BNE como um QA Analyst,
+  e resolvi bugs usando linguagem <span>SQL</span>.`
 };
+
