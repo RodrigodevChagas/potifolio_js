bbe22698f22594206b7c9c07de19ef31c4c1ec37
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-12 15:49:50 -0300

    alterando textos em ingles

--- public/js/resource/indexEn.js
+++ public/js/resource/indexEn.js
@@ -11,12 +11,12 @@ export const localizer = {
     <span>Bootstrap</span> and
     <span>JavaScript</span>.<br>In the second half of 2021, I put all my effort into
     learning more and more about this vast world of technology and, as I went
-    thexperirough new ences exploring projects like a <span>Login System</span>,
+    through new experiences exploring projects like a <span>Login System</span>,
     <span>Bank System</span>,
     <span>Crawler</span> application and built and sent request to some
     <span>APIs</span>
-    to name a few, as I'll present them in this project.<br>
-    Amongst the technologies I have experience with, here are some of them:
+    to name a few, I'll present them in this project.<br>
+    Amongst the technologies I have experience with, are:
     <span>SQLServer</span>, <span>RabbitMQ</span>, <span>Elasticsearch</span>,
     <span>Kibana</span>, <span>MLS</span>, <span>Amazon Web Services</span>, <span>Azure
     DevOpps</span> and <span>Blobb Storage</span>, <span>Git and GitHub</span> and
@@ -32,7 +32,7 @@ export const localizer = {
     <span>SOLID</span>.<br>
     On August 2022 I joined "Employer" as an intern, and I'm still working there at the
     moment as part of the BNE squad, where I learned how a big company development
-    environment works, how to deal with and solve problems.<br>
+    environment works, how to solve and deal with problems.<br>
     Besides programming and it's huge world of possibilities, some of my qualities I can
     name briefly are: I'm an <span>fully English speaker</span> and that I see myself as
     a hustler, proactive man who never gives up and loves to breakdown complicated
@@ -43,18 +43,17 @@ export const localizer = {
     Tellus
     , from January of 2020 to April 2021, going from house to house, talking to hundreds of
     people and handing their light bill.
-    There I learned how to communicate better, exercised my empathy, my patience (delivering
+    There I learned how to communicate better, have empathy, and to be patience (delivering
     debts to people is not easy), but
     mostly how to value work and take it as seriously as possible.<br>
     Currently I am working at Employer, as a intern and part of the BNE squad, which detains 2
     of the 2 biggest website of Human Resources
     in Brazil. There I got to experience how to deal with software in a professional level, as
-    I: delivered some projects
-    (<span>Catalog, CRUD, Shopping Cart</span>), collaborated to a new <span>tool of
-        management</span> of one of the company's websites, had contact with the company's
+    I: delivered some projects <span>Catalog, CRUD, Shopping Cart</span>, collaborated to a new <span>tool of
+    management</span> of one of the company's websites, had contact with the company's
     <span>Crawler</span> (gets jobs from other sites and publishes them in our) using XPath
     expressions to get analysis of HTML or XML files and collecting
     data, indexed files into a <span>LMS</span> website using <span>AWS services</span>,
     documented and did test cases of pretty much the whole BNE site as QA Analyst,
-    and solved quick fix bugs by altering fields in the production database.`
+    and solved quick fix bugs by altering fields in the production database using <span>SQL</span>.`
 };
