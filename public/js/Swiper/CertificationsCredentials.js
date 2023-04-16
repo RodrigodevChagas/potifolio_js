d030f31c01784f313f33e8d0e5f9016f7d158cf0
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-16 16:40:32 -0300

    isolando as credenciais que seriam as mesmas

---
+++ public/js/Swiper/CertificationsCredentials.js
@@ -0,0 +1,37 @@
+export const cSharpCertificationsCredentials = [
+    "/degree/certificate/bd1bbfb4-d7fd-4ad6-8492-d5c9bee80b88",
+    "e2aaa9c3-5c82-4829-84b5-c2db82fef38f",
+    "3b562fc6-d049-4d4b-aec7-272f338a17fe",
+    "7949fe59-5f0f-411a-849e-577f112e8578",
+    "660b9e5f-332f-4166-b16e-982e7d0493e1",
+    "330cea66-219b-499d-9960-4895bf4e1ab3",
+    "2b1bbf30-0c3e-42dd-87e6-9f283ba3ee79",
+    "77955027-4523-4901-9ca9-5630c6df4d81",
+    "9564c431-efdd-47ee-931f-efd1ba26e087",
+    "184a5756-6990-4b82-bafd-dd5edb781194",
+    "7b3c7bd3-ef3e-4992-845f-8effeed19dcd",
+    "e2efc085-9090-44cf-bfac-7afa57089153",
+    "55e7695d-3391-4c05-a0f5-8a6b99f4143e",
+];
+
+export const pythonCertificationsCredentials = [
+    "/degree/certificate/f743efa3-031b-46d8-8ff2-88bc0597dfb5",
+    "86a59d85-25c1-4e6e-b003-7847a0f02c1d",
+    "85d00f10-fff7-4c58-b5f4-bf67cb60e88a",
+    "accb4319-e3db-45f2-aa21-7e853e3639e1",
+    "c0468f78-c17e-4661-9e6a-1046ee3706c5",
+    "71db1d52-f976-44b7-ac26-70f04611e324",
+    "8ad736cb-23d6-4a42-a76d-936e9b8931f4",
+    "ec8234dc-67f0-4ad0-8f36-56c4a0144ad6",
+    "3522b6dc-aec3-4c6d-9827-6fa6dbe0da15",
+    "610f4ab9-a23e-4dd6-b2ab-a3c522c681ec",
+];
+
+export const frontEndCertificationsCredentials = [
+    "e25f7532-d331-42b3-8876-7cdf6b8c9c03",
+    "c08d9993-8e95-4182-8328-1ff8001ff81c",
+    "f9d5b2c-cd79-49fb-b60d-55e96a81fd3d",
+    "893cc341-ff19-48fc-aec1-49f459311570",
+    "03f35994-8c34-495e-9789-a9b8184d1d95",
+    "7809629f-0d86-4e51-9bc2-312d2782e55a",
+];
