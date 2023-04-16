7549a15779b9942b1d538d995aabb1275fffcec4
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-16 16:40:45 -0300

    certificacoes em portugues

---
+++ public/js/Swiper/CertificationsPt.js
@@ -0,0 +1,41 @@
+const cSharpCertificationsPT = [
+  "C# e orientação a objetos",
+  ".NET 5 e EF Core: Relacionando Entidades",
+  "API Rest com .NET 5: Operações essenciais com verbos HTTP",
+  "C# parte 1: Primeiros passos",
+  "C# parte 2: Entendendo a Orientação a Objetos",
+  "C# parte 3: entendendo herança e interface",
+  "C# parte 4: Entendendo exceções",
+  "C# parte 5: Bibliotecas DLLs, documentação e usando o NuGet",
+  "C# parte 6: Strings, expressões regulares e a classe Object",
+  "C# parte 7: Array e tipos genéricos",
+  "C# parte 8: List, lambda, linq",
+  "C# Parte 9: entrada e saída (I/O) com streams",
+  "HTTP: Entendendo a web por baixo dos panos",
+]
+
+const pythonCertificationsPT = [
+  "Aprenda a programar em Python com Orientação a Objetos",
+  "Python Brasil: validação de dados no padrão nacional",
+  "Testes automatizados: TDD com Python",
+  "Python Collections parte 1: listas e tuplas",
+  "Python Collections parte 2: conjuntos e dicionários",
+  "Python: avançando na linguagem",
+  "Python: Avançando na orientação a objetos",
+  "Python: começando com a linguagem",
+  "Python: entendendo a Orientação a Objetos",
+  "String em Python: extraindo informações de uma URL",
+];
+
+const frontEndCertificationsPT = [
+  "Bootstrap 4: criando uma landing page responsiva",
+  "JavaScript: Conhecendo o Browser e padrões de projeto",
+  "HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas",
+  "HTML5 e CSS3 parte 1: crie uma página da Web",
+  "HTML5 e CSS3 parte 2: Posicionamento, listas e navegação",
+  "HTML5 e CSS3 parte 4: Avançando no CSS",
+];
+
+export const cSharpCertifications = cSharpCertificationsPT;
+export const pythonCertifications = pythonCertificationsPT;
+export const frontEndCertifications = frontEndCertificationsPT;
